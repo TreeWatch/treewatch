60b7c18c3273d46a15f13b854f3bb01dc2e00819
Mazorius <mail@ron.gebauers.org>
2015-10-06 15:33:09 +0200

    change branch

---
+++ SettingsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TreeWatch
+{
+	public partial class SettingsPage : ContentPage
+	{
+		public SettingsPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
