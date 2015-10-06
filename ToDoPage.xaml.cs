72ee82279c11c3c619b5eec676eeb7a7f2cb0054
Mazorius <mail@ron.gebauers.org>
2015-10-06 15:26:32 +0200

    create tabbedpage

---
+++ ToDoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TreeWatch
+{
+	public partial class ToDoPage : ContentPage
+	{
+		public ToDoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
