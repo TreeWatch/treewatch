405d62e85dcb76330f8d22a7e75e1e0b6e719144
Jan Kerkenhoff <Jan.Kerkenhoff@gmail.com>
2015-10-13 14:33:51 +0200

    cleaned up failed rebase

--- Pages/Map/MapContentPage.xaml.cs
+++ Pages/Map/MapContentPage.xaml.cs
@@ -14,29 +14,11 @@ namespace TreeWatch
 		{
 			InitializeComponent ();
 
-<<<<<<< Upstream, based on origin/master
-			#if __ANDROID__
-			this.Content = new ExtendedMap ();
-			#endif
-
-			//site configurations
-			Title = "Map";
-			NavigationPage.SetBackButtonTitle (this, Title);
-=======
 			NavigationPage.SetBackButtonTitle (this, "Back");
->>>>>>> ed096cc Added ExtendedMap Plugin to replace previous map This closes #5  #324 time:4 comment:Added ExtendedMap Plugin
 
-<<<<<<< Upstream, based on origin/master
-			//action after a field is clicked
-//			siteButton.Clicked += async (object sender, EventArgs e) => 
-//			{
-//				await this.Navigation.PushAsync(new OverlayContentPage());
-//			};
-=======
 			BindingContext = new MapViewModel ();
 
 			this.Content = CreateMapContentView ();
-
 		}
 
 		View CreateMapContentView ()
@@ -55,7 +37,6 @@ namespace TreeWatch
 			var createMapContentView = new CustomMapContentView (_map);
 
 			return createMapContentView;
->>>>>>> ed096cc Added ExtendedMap Plugin to replace previous map This closes #5  #324 time:4 comment:Added ExtendedMap Plugin
 		}
 	}
 }
