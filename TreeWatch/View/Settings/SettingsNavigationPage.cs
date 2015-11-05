7c7d8cac4c73867442805520050e01f466bb20e2
Ron Gebauer <mail@ron.gebauers.org>
2015-11-05 11:15:05 +0100

    add the stuff i forgot to commit in c356968

--- TreeWatch/View/Settings/SettingsNavigationPage.cs
+++ TreeWatch/View/Settings/SettingsNavigationPage.cs
@@ -7,7 +7,10 @@ namespace TreeWatch
 		public SettingsNavigationPage (Page root) : base (root)
 		{
 			Title = root.Title;
-			Icon = root.Icon;
+			if (root.Icon != null)
+			{
+				Icon = root.Icon;
+			}
 		}
 	}
 }
