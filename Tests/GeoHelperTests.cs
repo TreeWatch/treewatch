9624f467237d229ee6dee6c3c9471793c67410f7
renerisha <r.karoff@googlemail.com>
2015-11-05 13:52:02 +0100

    #336 comment: added GeoHelper class to check if a position is within a List of positions time:5

---
+++ Tests/GeoHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Xamarin.Forms.Maps;
+
+namespace TreeWatch.Tests
+{
+	[TestFixture ()]
+	public class GeoHelperTests
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+
+		[Test()]
+		public void checkInside()
+		{
+			List<Position> positionList 
+		}
+	}
+}
+
---
+++ TreeWatch/Utils/GeoHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Maps;
+
+namespace TreeWatch
+{
+	public static class GeoHelper
+	{
+		public static Boolean isInsideCoords(List<Position> coords, Position position){
+			Position p1, p2;
+
+			bool inside = false;
+
+			if (coords.Count < 3)
+			{
+				return inside;
+			}
+
+			var oldPos = new Position(
+				coords[coords.Count -1].Latitude, coords[coords.Count - 1].Longitude);
+
+			foreach(var pos in coords){
+
+				if (pos.Latitude > oldPos.Latitude) {
+					p1 = oldPos;
+					p2 = pos;
+
+				} else {
+					p1 = pos;
+					p2 = oldPos;
+				}
+
+				if ((pos.Latitude < position.Latitude) == (position.Latitude <= oldPos.Latitude)
+					&& (pos.Longitude - (long) p1.Longitude)*(p2.Latitude - p1.Latitude)
+					< (p2.Longitude - (long) p1.Longitude)*(pos.Latitude - p1.Latitude))
+				{
+					inside = !inside;
+				}
+
+				oldPos = pos;
+			}
+
+			return inside;
+		}
+	}
+}
+
