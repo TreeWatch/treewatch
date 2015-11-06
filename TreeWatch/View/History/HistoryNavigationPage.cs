3a22199b0dedff59b462189697ad8662cff3bbd8
Jan Kerkenhoff <Jan.Kerkenhoff@gmail.com>
2015-11-06 13:44:31 +0100

    fixed Tests

--- TreeWatch/View/History/HistoryNavigationPage.cs
+++ TreeWatch/View/History/HistoryNavigationPage.cs
@@ -7,14 +7,10 @@ namespace TreeWatch
 		public HistoryNavigationPage (Page root) : base (root)
 		{
 			Title = root.Title;
-<<<<<<< HEAD
 			if (root.Icon != null)
 			{
 				Icon = root.Icon;
 			}
-=======
-			Icon = root.Icon;
->>>>>>> 5ac15a78c018a0791722caa5014642cd1adabed3
 		}
 
 	}
