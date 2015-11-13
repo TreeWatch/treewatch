9af4b04db4623eb821481020fcc0c04108950e67
klotebandit <maxvanderlinden@hotmail.com>
2015-11-13 12:42:16 +0100

    Changed field to calculate pin position

--- TreeWatch/Model/Map/Field/Field.cs
+++ TreeWatch/Model/Map/Field/Field.cs
@@ -7,10 +7,23 @@ namespace TreeWatch
 {
 	public class Field
 	{
+		private List<Position> boundingCoordinates;
+		private Position fieldPinPosition;
 
 		public List<Position> BoundingCordinates {
+			get
+			{
+				return boundingCoordinates;
+			}
+			set
+			{
+				boundingCoordinates = value;
+				fieldPinPosition = CalculatePinPosition ();
+			}
+		}
+
+		public Position FieldPinPosition {
 			get;
-			set;
 		}
 
 		public List<Row> Rows {
@@ -25,9 +38,25 @@ namespace TreeWatch
 			this.Name = name;
 			BoundingCordinates = new List<Position> ();
 			Rows = new List<Row> ();
+			fieldPinPosition = new Position ();
 		}
 
+		private Position CalculatePinPosition()
+		{
+			var coordinateCounter = 0;
+			var latSum = 0.0;
+			var lonSum = 0.0;
 
+			foreach(Position fieldPoint in boundingCoordinates)
+			{
+				coordinateCounter++;
+				latSum += fieldPoint.Latitude;
+				lonSum += fieldPoint.Longitude;
+			}
+
+			return new Position (latSum/coordinateCounter, lonSum/coordinateCounter);
+
+		}
 	}
 }
 
