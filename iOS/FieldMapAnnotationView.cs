8279b54d1c11f2e5d59325d0e215f6f960ccd634
LasershowJack <martijnbonajo@gmail.com>
2015-11-13 15:15:18 +0100

    Added info button to pin popup

---
+++ iOS/FieldMapAnnotationView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MapKit;
+
+namespace TreeWatch.iOS
+{
+	public class FieldMapAnnotationView : MKAnnotationView
+	{
+		private readonly IMKAnnotation annotation;
+		private readonly string annotationId;
+
+		public FieldMapAnnotationView (IMKAnnotation annotation, string annotationId)
+		{
+			this.annotation = annotation;
+			this.annotationId = annotationId;
+			CanShowCallout = false;
+		}
+
+		public override void SetSelected(bool selected, bool animated)
+		{
+			base.SetSelected(selected, animated);
+
+			if (selected)
+			{
+				// show custom ui view
+			}
+			else
+			{
+				// hide custom ui view
+			}
+		}
+	}
+}
+
