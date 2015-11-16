67366470481c103f03bfd8a0e24d96ecd4aa9e2f
Jan Kerkenhoff <Jan.Kerkenhoff@gmail.com>
2015-11-16 23:06:58 +0100

    Fixed Tests Updated NUnit

--- TreeWatch.Tests/ColorHelperTest.cs
+++ TreeWatch.Tests/ColorHelperTest.cs
@@ -12,7 +12,8 @@ namespace TreeWatch.Tests
 		{
 			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.APPLE), Color.Lime);
 			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.PEAR), Color.Maroon);
-			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.CHERRY), Color.Purple);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.CHERRY), Color.Red);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.PLUM), Color.Purple);
 			Assert.AreEqual (ColorHelper.GetTreeTypeColor (TreeType.NOTDEFINED), Color.Black);
 		}
 
@@ -21,8 +22,10 @@ namespace TreeWatch.Tests
 		{
 			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("0"), Color.Lime);
 			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("1"), Color.Maroon);
-			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("2"), Color.Purple);
-			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("3"), Color.Black);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("2"), Color.Red);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("3"), Color.Purple);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("4"), Color.Black);
+			Assert.AreEqual (ColorHelper.GetTreeTypeColor ("5"), Color.Blue);
 		}
 	}
 }
