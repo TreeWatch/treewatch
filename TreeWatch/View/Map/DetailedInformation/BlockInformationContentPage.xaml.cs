7f266b8c0b26568aca0d0fc1d5c474c09c9795e6
Mazorius <mail@ron.gebauers.org>
2015-11-19 14:56:25 +0100

    upload disclosure indicator, add block information content page, update field information view model #348 time:1.5

---
+++ TreeWatch/View/Map/DetailedInformation/BlockInformationContentPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TreeWatch
+{
+	public partial class BlockInformationContentPage : ContentPage
+	{
+		public BlockInformationContentPage (FieldInformationViewModel fieldInformationViewModel)
+		{
+			// initialize component
+			InitializeComponent ();
+
+			// set view model
+			this.BindingContext = fieldInformationViewModel;
+		}
+	}
+}
+
