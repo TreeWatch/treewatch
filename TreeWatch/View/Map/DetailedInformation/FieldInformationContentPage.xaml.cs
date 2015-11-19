d26d3ca70bd639e4a17a6c14475d511fc045d3dc
Mazorius <mail@ron.gebauers.org>
2015-11-19 16:01:08 +0100

    change title of back button in navigation #348 time:1

--- TreeWatch/View/Map/DetailedInformation/FieldInformationContentPage.xaml.cs
+++ TreeWatch/View/Map/DetailedInformation/FieldInformationContentPage.xaml.cs
@@ -1,16 +1,22 @@
 ﻿using Xamarin.Forms;
+using System;
+using System.Diagnostics;
 
 namespace TreeWatch
 {
 	public partial class FieldInformationContentPage : ContentPage
 	{
-		public FieldInformationContentPage (InformationViewModel fieldInformationViewModel)
+		public FieldInformationContentPage (InformationViewModel informationViewModel)
 		{
 			// initialize component
 			InitializeComponent ();
 
 			// set view model
-			this.BindingContext = fieldInformationViewModel;
+			this.BindingContext = informationViewModel;
+
+			NavigationPage.SetBackButtonTitle (this, informationViewModel.FieldName);
+
+			blocks.Tapped += (sender, e) => ((InformationViewModel)this.BindingContext).NavigateToBlocks();
 		}
 	}
 }
