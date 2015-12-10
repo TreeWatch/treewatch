adc4d41d0a9d1dc4898f584454e838b70a182836
Mazorius <mail@ron.gebauers.org>
2015-12-10 13:41:28 +0100

    remove layer items from toolbar because task is canceled

--- TreeWatch/View/Map/MapContentPage.xaml.cs
+++ TreeWatch/View/Map/MapContentPage.xaml.cs
@@ -10,35 +10,67 @@ namespace TreeWatch
             // initialize this page
             InitializeComponent();
 
-            // add view model
-            BindingContext = mapViewModel;
-
-            setupMapContentView();
-
             if (TargetPlatform.Android == Device.OS)
             {
                 Title = "Map";
             }
 
+            // add view model
+            BindingContext = mapViewModel;
+
+            setupMapContentView();
+
+            setupToolbarItems();
+        }
+
+        /**
+         * This method configurate the field map view inside the xaml
+         */
+        internal void setupMapContentView()
+        {
+            // Get binding context of this view
+            var viewModel = BindingContext as MapViewModel;
+            // Get current position
+            var currentLocation = viewModel.getCurrentDevicePosition();
+
+            // Jump to the current location inside the map
+            fieldMap.MoveToRegion(MapSpan.FromCenterAndRadius(currentLocation, Distance.FromKilometers(1)));
+
+            // Add all fields into the map
+            fieldMap.Fields = viewModel.Fields;
+
+            // Change Map type to hybrid
+            fieldMap.MapType = MapType.Hybrid;
+
+            // set binding context of field map view to the same of this view
+            fieldMap.BindingContext = BindingContext;
+        }
+
+        /**
+         * This method create and configurate all toolbar items
+         */
+        internal void setupToolbarItems()
+        {
             if (TargetPlatform.iOS == Device.OS)
             {
-                var myLocationToolBarItem = new ToolbarItem();
-                myLocationToolBarItem.Icon = Device.OS == TargetPlatform.iOS ? "Icons/MyLocationIcon.png" : "MyLocationIcon.png";
-                ToolbarItems.Insert(0, myLocationToolBarItem);
+                ToolbarItems.Insert(0, getMyLocationToolbarItem());
             }
         }
 
-        void setupMapContentView()
+        /**
+         * This method create the toolbar item that should jump the map view to current location
+         */
+        internal static ToolbarItem getMyLocationToolbarItem()
         {
-            var viewModel = BindingContext as MapViewModel;
-            var currentLocation = viewModel.getCurrentDevicePosition();
+            var myLocationToolBarItem = new ToolbarItem();
 
-            fieldMap.MoveToRegion(MapSpan.FromCenterAndRadius(currentLocation, Distance.FromKilometers(1)));
-            fieldMap.Fields = viewModel.Fields;
+            // Design Toolbar Item
+            myLocationToolBarItem.Icon = Device.OS == TargetPlatform.iOS ? "Icons/MyLocationIcon.png" : "MyLocationIcon.png";
 
-            fieldMap.MapType = MapType.Hybrid;
+            // Set clicked event
+            myLocationToolBarItem.Clicked += (sender, e) => System.Diagnostics.Debug.WriteLine("Jump to my postion …");
 
-            fieldMap.BindingContext = BindingContext;
+            return myLocationToolBarItem;
         }
     }
 }
